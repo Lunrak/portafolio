8f5ce3ae9fd7736a0f9150b0bbd14c4b6c8a59d7
Hansel Rodriguez Vazquez <hansel.lunrak@gmail.com>
2026-08-22 15:44:54 -0400

    feat:iniciar To-Do App con estructura HTML, CSS y variables JavaScript

---
+++ todo-app/app.js
@@ -0,0 +1,15 @@
+const formularioTarea = document.getElementById("formulario-tarea");
+const inputTarea = document.getElementById("input-tarea");
+const listaTareas = document.getElementById("lista-tareas");
+const contadorTareas = document.getElementById("Contador-tareas");
+const mensajeVacio = document.getElementById("mensaje-vacio");
+
+// para guardar las tareas
+const tareas = [];
+
+// Contador de tareas
+let contador = 0;
+
+console.log("cargada correctamente");
+console.log("tareas iniciales:", tareas);
+console.log("Contador inicial", contador);
